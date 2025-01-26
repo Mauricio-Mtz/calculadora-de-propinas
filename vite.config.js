8f796a7b40c1960d8dfc6d20cf15d4ecfc5811d4
Mauricio <136762122+Mauricio-Mtz@users.noreply.github.com>
2025-01-26 09:51:39 -0600

    Correccion de configuracion dos

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 10000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: 10000,
     host: '0.0.0.0',
-    preview: {
-      allowedHosts: ['calculadora-de-propinas.onrender.com']
-    }
+    allowedHosts: ['calculadora-de-propinas.onrender.com']
   }
 })
