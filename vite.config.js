32c444804988c304779bbb24b7ae6e6499dc0865
Mauricio <136762122+Mauricio-Mtz@users.noreply.github.com>
2025-01-26 00:45:35 -0600

    Correccion de las rutas permitidas

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     port: 10000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    preview: {
+      allowedHosts: ['calculadora-de-propinas.onrender.com']
+    }
   }
 })
